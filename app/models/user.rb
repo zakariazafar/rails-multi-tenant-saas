46fc2fbf4057780d84f235a8cb7b794e2082734e
Abu <zakariazafar@yahoo.com>
2016-01-06 21:42:37 +1100

    sending confirmation email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ActiveRecord::Base
 
   validate :email_is_unique, on: :create
   after_create :create_account
-  def confirmation_required?
-  		false
-  end
+  
+  # def confirmation_required?
+  # 		false
+  # end
 
   private
 
