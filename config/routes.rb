5998ebaa323bd4c0e636555948c45eb803c5aa72
Abu <zakariazafar@yahoo.com>
2016-01-13 03:20:27 +1100

    upgrade/downgrade subscription and update credit card

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :weather_grid
+  get "/subscriptions/cancel_subscription"   => "subscriptions#cancel_subscription"
+  get "/subscriptions/update_card"   => "subscriptions#update_card"
+  post "/subscriptions/update_card_details"   => "subscriptions#update_card_details"
   resources :subscriptions
+
   root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
